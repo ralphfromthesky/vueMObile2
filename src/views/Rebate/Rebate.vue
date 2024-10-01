9fea90718d996ed9b8b2acf478b95b220e3ed7c1
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-10-01 11:25:07 +0800

    new

--- src/views/Rebate/Rebate.vue
+++ src/views/Rebate/Rebate.vue
@@ -2,7 +2,7 @@
   <div class="p-1 text-white">
     <div class="flex gap-1">
       <div>
-        <div v-for="(e, index) in titleLink" :key="index" class="my-1">
+        <div v-for="(e, index) in titleLink" :key="index" class="my-1 border-">
           <AntButton
             :title="e.title"
             :bg="activeBtn === index ? ' #05309f' : '#FFF0BB'"
@@ -12,36 +12,42 @@
             w="1.5rem"
             b=".1rem"
             ft=".22rem"
-            @click="showActiviy(index, e.title, e.type )"
+            @click="showActiviy(index, e.title, e.type)"
           />
         </div>
       </div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'Slots'">
-      <div class="bg-bg2 p-[.2rem] flex justify-between rounded-[.1rem]">
-        <div class="">
-          <div class="text-bg font-bold text-[.3rem]">{{activeTitle}}</div>
-          <div class="my-1 p-[.1rem] rounded-[.2rem] bg-bg text-txt">Bet - {{ kickBack?.data?.content?.rows[0].minBet }} /  get - {{0.01 * kickBack?.data?.content?.rows[0].kickback }}</div>
-
+      <div
+        class="h-[13rem] flex-auto p-1 overflow-auto"
+        v-if="activeTitle"
+      >
+        <div class="bg-bg p-[.2rem] flex justify-between rounded-[.1rem] text-txt">
+          <div class="text-txt">
+            <div class="text-bg text-txt font-bold text-[.3rem]">{{ activeTitle }}</div>
+            <div class="my-[.1rem] p-[.1rem] rounded-[.2rem] bg-txt text-bg">
+              Bet - {{ kickBack?.data?.content?.rows[0].minBet }} / get -
+              {{ kickBack?.data?.content?.rows[0].kickback }}
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <span>Rebate Max 0%</span>
+            <span>Limit</span>
+            <span class="text-bg text-txt font-bold">Rebate {{ 0.01 * kickBack?.data?.content?.rows[0].kickback  }}</span>
+          </div>
         </div>
-        <div class="flex flex-col">
-          <span>Rebate Max</span>
-          <span>Limit</span>
-          <span>Rebate</span>
+        <div class="border-[.01rem] rounded-[.1rem] p-1 overflow-hidden my-[.1rem] border-bord flex flex-col items-center h-[10rem]">
+          <img
+              class="w-[2.5rem] text-txt"
+              src="/nodataImages/img_none_jl.png"
+              alt=""
+            />
+          
+            <span>No data for {{ activeTitle }} for now</span>
         </div>
       </div>
-      
-      </div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'Fishing'">{{activeTitle}}2</div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'Live Casino'">{{activeTitle}}3</div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'E-Sports'">{{activeTitle}}4</div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'Sports'">{{activeTitle}}5</div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'Lottery'">{{activeTitle}}5</div>
-      <div class="h-[13rem] flex-auto p-1 overflow-auto" v-if="activeTitle === 'iGaming'">{{activeTitle}}6</div>
 
-      
     </div>
 
-    <!-- <<SpinLoader/> -->
+    <!-- <SpinLoader/> -->
   </div>
 </template>
 
@@ -51,22 +57,21 @@ import { useMutation, useQuery } from "@tanstack/vue-query";
 import { axiosGet, axiosPost } from "@/components/axios/AxiosHook";
 
 const activeBtn = ref(0);
-const activeTitle = ref('Slots')
-const kickBack = ref([])
+const activeTitle = ref("Slots");
+const kickBack = ref([]);
 
 const showActiviy = (index, title, type) => {
-    activeBtn.value = index
-    activeTitle.value = title
-    mutate({
-      type: type,
-      pageSize: 100
-    })
-    
-}
+  activeBtn.value = index;
+  activeTitle.value = title;
+  mutate({
+    type: type,
+    pageSize: 100,
+  });
+};
 
 const titleLink = ref([
   { title: "Slots", type: 2 },
-  { title: "Fishing" , type: 4 },
+  { title: "Fishing", type: 4 },
   { title: "Live Casino", type: 1 },
   { title: "E-Sports", type: 6 },
 
@@ -74,15 +79,17 @@ const titleLink = ref([
 
   { title: "Lottery", type: 7 },
 
-  { title: "iGaming",type: 4 },
+  { title: "iGaming", type: 4 },
 ]);
 
-const {mutate, isSuccesss} = useMutation({
-  mutationFn: (payload) => axiosPost('/api/native/v2/getKickBackStrategy.do', payload),
-  onSuccess: (data) => {kickBack.value = data},
+const { mutate, isSuccesss } = useMutation({
+  mutationFn: (payload) =>
+    axiosPost("/api/native/v2/getKickBackStrategy.do", payload),
+  onSuccess: (data) => {
+    kickBack.value = data;
+  },
   onError: (err) => alert(err),
-  
-})
+});
 </script>
 
 <style lang="scss" scoped></style>
